a26991477dc2834ef33c9a0ee11f2021ed104e4e
MdAdnanMorshed <adnanmorshed118@gmail.com>
2023-07-22 08:39:00 +0600

    task4 UI Modify

--- lib/app/modules/task4/views/task4_view.dart
+++ lib/app/modules/task4/views/task4_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../data/models/ticket_dt_model.dart';
+import '../../../data/utils/app_space.dart';
 import '../../../data/utils/shimmer_effect.dart';
 import '../controllers/task4_controller.dart';
 
@@ -29,17 +30,131 @@ class Task4View extends GetView<Task4Controller> {
               itemCount: controller.dataDTList.length,
               itemBuilder: (BuildContext context, int index) {
                 TicketDTModel data = controller.dataDTList[index];
-                return Card(
-                  child: Container(
-                      margin: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
-                      padding: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
-                      child: Text(data.toJson().toString())),
-                );
+                return _dtDataWidget(data);
               },
             );
           }
         }
       }),
+    );
+  }
+  _dtDataWidget(TicketDTModel data) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
+        margin: const EdgeInsets.only(left: 10, right: 10, top: 2),
+
+        width: Get.size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white.withOpacity(0.9),
+        ),
+        child: Container(
+
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Row(
+                children: [
+                  const Text(
+                    'Type : ',
+                    style: TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    data.dsc.toString(),
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+
+              Row(
+                children: [
+                  const Text(
+                    'app_avail : ',
+                    style: TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    data.appAvail.toString(),
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+              AppSpace.spaceH4,
+              Row(
+                children: [
+                  const Text(
+                    'Quantity  : ',
+                    style: TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                   data.qty.toString(),
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+              AppSpace.spaceH4,
+              Row(
+                children: [
+                  const Text(
+                    'Discount  : ',
+                    style: TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    ' ৳ '+ data.discount.toString(),
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+
+              AppSpace.spaceH4,
+              Row(
+                children: [
+                  const Text(
+                    'Vat  : ',
+                    style: TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    ' ৳ '+ data.vat.toString(),
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+
+              AppSpace.spaceH4,
+              const Divider(thickness: 1, height: 1,color: Colors.grey,),
+
+              Row(
+                children: [
+                  const Text(
+                    'MRP : ',
+                    style: TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    ' ৳ '+data.mrp.toString(),
+                    style: const TextStyle(
+                        fontSize: 12, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
